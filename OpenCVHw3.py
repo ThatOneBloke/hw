3e14ef7711c2f68779f13f4bd8cb5d1f0814235c
ThatOneBloke <x1648811@gmail.com>
2026-06-18 17:05:56 +0100

    Add files via upload

--- OpenCVHw3.py
+++ OpenCVHw3.py
@@ -27,6 +27,69 @@ image1 = cv2.putText(image1, "Eye", TextStartingPoint, Font, FontScale, Colour, 
 image1 = cv2.line(image1, LineStartingPoint, EndingPoint, Colour, Thickness)
 #This creates the line on the image.
 
+LineStartingPoint = (215, 120)
+EndingPoint = (265, 120)
+Colour = (0, 0, 245)
+Thickness = 5
+
+Font = cv2.FONT_HERSHEY_COMPLEX
+TextStartingPoint = (260, 120)
+FontScale = 1
+Colour = (0, 0, 255)
+Thickness = 1
+image1 = cv2.putText(image1, "mouth", TextStartingPoint, Font, FontScale, Colour, Thickness)
+
+image1 = cv2.line(image1, LineStartingPoint, EndingPoint, Colour, Thickness)
+
+LineStartingPoint = (215, 50)
+EndingPoint = (265, 50)
+Colour = (0, 0, 245)
+Thickness = 5
+
+Font = cv2.FONT_HERSHEY_COMPLEX
+TextStartingPoint = (260, 50)
+FontScale = 1
+Colour = (0, 0, 255)
+Thickness = 1
+image1 = cv2.putText(image1, "Brain", TextStartingPoint, Font, FontScale, Colour, Thickness)
+
+image1 = cv2.line(image1, LineStartingPoint, EndingPoint, Colour, Thickness)
+
+LineStartingPoint = (265, 155)
+EndingPoint = (285, 250)
+Colour = (0, 0, 245)
+Thickness = 5
+
+Font = cv2.FONT_HERSHEY_COMPLEX
+TextStartingPoint = (260, 150)
+FontScale = 1
+Colour = (0, 0, 255)
+Thickness = 1
+image1 = cv2.putText(image1, "bone", TextStartingPoint, Font, FontScale, Colour, Thickness)
+
+image1 = cv2.line(image1, LineStartingPoint, EndingPoint, Colour, Thickness)
+
+LineStartingPoint = (270, 610)
+EndingPoint = (280, 600)
+Colour = (0, 0, 245)
+Thickness = 5
+
+Font = cv2.FONT_HERSHEY_COMPLEX
+TextStartingPoint = (280, 600)
+FontScale = 1
+Colour = (0, 0, 255)
+Thickness = 1
+image1 = cv2.putText(image1, "limbs", TextStartingPoint, Font, FontScale, Colour, Thickness)
+
+image1 = cv2.line(image1, LineStartingPoint, EndingPoint, Colour, Thickness)
+
+LineStartingPoint = (300, 450)
+EndingPoint = (290, 575)
+Colour = (0, 0, 245)
+Thickness = 1
+
+image1 = cv2.line(image1, LineStartingPoint, EndingPoint, Colour, Thickness)
+
 cv2.imshow("Label anatomy", image1)
 
 cv2.waitKey(0)
